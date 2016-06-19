db959e029d291f1e85826e75550af4e8e4f617a1
PeterTheDestroyer <peter.lee@berkeley.edu>
2016-06-19 17:55:46 -0500

    Fixed broken links

--- js/partials.js
+++ js/partials.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   /* Import Partials */
-  $('nav').load('../partials/navbar.html');
-  $('section#service').load('../partials/service.html');
-  $('section#portfolio').load('../partials/portfolio.html');
-  $('footer').load('../partials/footer.html');
+  $('nav').load('partials/navbar.html');
+  $('section#service').load('partials/service.html');
+  $('section#portfolio').load('partials/portfolio.html');
+  $('footer').load('partials/footer.html');
 });
