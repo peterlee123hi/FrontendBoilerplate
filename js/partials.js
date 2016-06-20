f82d72872e81ef70c813926c05f78f10a3be7d32
PeterTheDestroyer <peter.lee@berkeley.edu>
2016-06-19 19:06:07 -0500

    Added sample website

--- js/partials.js
+++ js/partials.js
@@ -3,5 +3,6 @@ $(document).ready(function() {
   $('nav').load('partials/navbar.html');
   $('section#service').load('partials/service.html');
   $('section#portfolio').load('partials/portfolio.html');
+  $('section#team').load('partials/team.html');
   $('footer').load('partials/footer.html');
 });
